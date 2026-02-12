feed6e05f3b3b34bf621043121ed348b81e0cc71
arianaramirezusf <arianaramirez@usf.edu>
2026-02-12 12:52:24 -0500

    Creating Array of Objects

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+//Creating Array of Objects
+let products = [
+    {name: "Blush", category: "Makeup", price: 7.99, inventory: 15},
+    {name: "Headphones", category: "Electronics", price: 32.99, inventory: 20},
+    {name: "Sweater", category: "Clothing", price: 25.99, inventory: 30},
+    {name: "Cereal", category: "Grocery", price: 5.99, inventory: 25},
+    {name: "Purse", category: "Accessories", price: 19.99, inventory: 10},
+]
+
+
